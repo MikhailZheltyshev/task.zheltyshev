68cfa086f4225fb78789663e4dadb6ee858ffa53
mike_zhel <mihalich93@bk.ru>
2020-01-23 20:29:57 +0300

    added first methods to main page PO

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MainPage extends BasePage {
 
     public static final String URL = baseUrl + "/main";
@@ -15,7 +17,7 @@ public class MainPage extends BasePage {
     private WebElement logoutButton;
 
     @FindBy(xpath = "//div[text()='Список дел']")
-    private WebElement toDoListTitle;
+    private WebElement tasksListTitle;
 
     @FindBy(xpath = "//div[text()='Управление']")
     private WebElement taskInputTitle;
@@ -28,5 +30,69 @@ public class MainPage extends BasePage {
 
     public MainPage(WebDriver driver) {
         super(driver);
+    }
+
+    public void clickOnLogOutButton() {
+        logoutButton.click();
+    }
+
+    public void clickOnAddTaskButton() {
+        addTaskButton.click();
+    }
+
+    public void sendKeysToTaskInputField(String taskText) {
+        taskInputField.sendKeys(taskText);
+    }
+
+    public void clearTaskInputField() {
+        taskInputField.clear();
+    }
+
+    public void addTask(String taskText) {
+        taskInputField.clear();
+        taskInputField.sendKeys(taskText);
+        addTaskButton.click();
+    }
+
+    public void checkLogOutButtonIsDisplayed() {
+        assertThat(logoutButton.isDisplayed())
+                .as("Logout button should be displayed")
+                .isTrue();
+    }
+
+    public void checkLogOutButtonLabelEqualsTo(String expectedText) {
+        assertThat(logoutButton.getText())
+                .as("Logout button should has [{}] label", expectedText)
+                .isEqualTo(expectedText);
+    }
+
+    public void checkHeaderPanelIsDisplayed() {
+        assertThat(header.isDisplayed())
+                .as("Header panel should be displayed")
+                .isTrue();
+    }
+
+    public void checkTasksListTitleIsDisplayed() {
+        assertThat(tasksListTitle.isDisplayed())
+                .as("Tasks list title should be displayed")
+                .isTrue();
+    }
+
+    public void checkTasksListTitleEqualsTo(String expectedText) {
+        assertThat(tasksListTitle.getText())
+                .as("Tasks list title should be equal to [{}]", expectedText)
+                .isEqualTo(expectedText);
+    }
+
+    public void checkTaskInputFieldIsDisplayed() {
+        assertThat(taskInputField.isDisplayed())
+                .as("Task input field should be displayed")
+                .isTrue();
+    }
+
+    public void checkAddTaskButtonIsDisplayed() {
+        assertThat(addTaskButton.isDisplayed())
+                .as("Add task button should be displayed")
+                .isTrue();
     }
 }
